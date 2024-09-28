d52365edd36a532d96ce699c0bd42e8f4b5c0096
Erwandi Pramudya <epramudya@aio.co.id>
2024-09-28 23:34:10 +0700

    fix get groups

--- src/routes/master/groups/index.ts
+++ src/routes/master/groups/index.ts
@@ -5,11 +5,7 @@ import { db1 } from "../../../utils/db1";
 export const get = [async (req: Request, res: Response) => {
   if (req.method !== "GET") return res.status(405);
   
-  const data = await db1.mst_group.findMany({
-    where: {
-      is_deleted: false
-    }
-  })
+  const data = await db1.mst_group.findMany()
   // const data = await db1.mst_group.findUnique({
   //   where: {
   //     is_deleted: 0,
