5e849d0a9c152ee4dcc4cc9656a63a6ce548a557
Hilman Abdurrahman Achmad <hachmad@aio.co.id>
2024-09-30 10:16:23 +0700

    git remove cache to push .gitignore

--- src/utils/db2.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient as  PrismaClient2} from "../generated/db2";
-
-export const db2 = new PrismaClient2();
--- src/utils/db3.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient as  PrismaClient2} from "../generated/db3";
-
-export const db3 = new PrismaClient2();
