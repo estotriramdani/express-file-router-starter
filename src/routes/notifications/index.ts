d4f1a878789a2be73d74323431253a97449ba491
Esto Triramdani N <estolagi@gmail.com>
2024-10-05 16:39:24 +0700

    Refactor notifications route to fetch notifications for specific employee

--- src/routes/notifications/index.ts
+++ src/routes/notifications/index.ts
@@ -1,16 +1,20 @@
 import type { Response } from 'express';
 import { authenticateJWT } from '@/middlewares/bearerToken';
 import { ExtendedRequest } from '@/types/auth';
+import { db1 } from '@/utils/db1';
 
 export const post = async (req: ExtendedRequest, res: Response) => {};
 
 export const get = [
   authenticateJWT,
   async (req: ExtendedRequest, res: Response) => {
-    res.json({ message: req.user.employment });
+    const data = await db1.tr_notification.findMany({
+      where: {
+        employee_code: {
+          endsWith: req?.user?.employment?.employee_code,
+        },
+      },
+    });
+    res.json({ status: true, data: data });
   },
 ];
-
-export const put = async (req: ExtendedRequest, res: Response) => {};
-
-export const del = async (req: ExtendedRequest, res: Response) => {};
