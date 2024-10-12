b9b42860a81aa60e9583ad9cdd965f2dedb9c04b
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-10-12 13:24:41 +0700

    first templating

--- src/views/validation/index.ts
+++ src/views/validation/index.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs';
-
+import { fillParameters } from '@/utils';
 
 export const get = async (req: Request, res: Response) => {
-  const template = fs.readFileSync(path.join(__dirname, '../email/bodyEmailRequestValidation.html'), 'utf8');
-  const html = template.replace('{{name}}', 'John Doe');
-  
+  const template = fs.readFileSync(path.join(__dirname, './template.html'), 'utf8');
+
+  const html = fillParameters(template, {
+    name: 'Validation',
+    description: 'This is a validation page',
+  });
+
   res.send(html);
-}
+};
+
+export const post = async (req: Request, res: Response) => {
+  const { name } = req.body;
+
+  console.log(name);
+
+  res.send({ name });
+};
