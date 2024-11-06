66abbab2f45aa4a4340ff46b6ae0568c69c59385
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-11-06 09:06:07 +0700

    feat: update access token expiration based on environment mode

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,5 +2,5 @@ import { UserResponse } from '@/types/auth';
 import jwt from 'jsonwebtoken';
 
 export function generateAccessToken(user: UserResponse) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.MODE === 'development' ? '1d' : '1h' });
 }
