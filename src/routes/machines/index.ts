73e060222ff874ac7a0aa00ce69d9555ffbf74f5
Esto Triramdani N <estolagi@gmail.com>
2024-11-27 10:10:06 +0700

    feat: enhance GET route for machines to include self links and transform response data

--- src/routes/machines/index.ts
+++ src/routes/machines/index.ts
@@ -7,8 +7,29 @@ export const get = [
   authenticateJWT,
   async (req: ExtendedRequest, res: Response) => {
     try {
+      const links = {
+        self: process.env.SELF_URL + req.originalUrl,
+      };
+
       const data = await digital_twin_db.mst_machine.findMany();
-      res.json({ status: true, data });
+
+      const transformed = data.map((item) => {
+        return {
+          type: 'machines',
+          id: item.id,
+          attributes: {
+            slug: item.slug,
+            name: item.name,
+            createdAt: item.createdAt,
+            updatedAt: item.updatedAt,
+          },
+        };
+      });
+
+      res.json({
+        links,
+        data: transformed,
+      });
     } catch (error) {
       res.status(500).json({ error });
     }
