c25828c631ead4628280202ee60b797869d888ff
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-11-28 09:13:24 +0700

    fix: update user type in ExtendedRequest interface for better type safety

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -93,7 +93,7 @@ export type UserResponse = {
 };
 
 export interface ExtendedRequest extends Request {
-  user?: UserResponse;
+  user?: LoginDataAttributes;
 }
 
 export interface LoginDataAttributes {
