2a9a06d02115201afb32b3e160ce5812e2a9baa9
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-12-09 16:17:24 +0700

    feat: enhance GET endpoint for warehouses to include detailed occupancy data and update token expiration to 1 hour

--- src/routes/warehouses/index.ts
+++ src/routes/warehouses/index.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 import { authenticateJWT } from '@/middlewares/bearerToken';
 import { generateError, generateRandomString } from '@/utils';
 import { catchResponse } from '@/utils/response';
+import { dwh_db } from '@/lib/db';
 
 export const get = [
   authenticateJWT,
@@ -11,11 +12,87 @@ export const get = [
         self: process.env.SELF_URL + req.originalUrl,
       };
 
-      const data = [];
+      type Data = {
+        WarehouseGroup: string;
+        StockConv: number;
+        Capacity: number;
+        SCORE: number;
+        SCORE_CONV: number;
+      };
+
+      const data = await dwh_db.$queryRaw<Data[]>`SELECT TOP (1000) [WarehouseGroup]
+      ,[Stock Conv]
+      ,[Capacity]
+      ,[SCORE]
+      ,[SCORE_CONV]
+  FROM [BI_DASHBOARD].[dbo].[Tb_STOCK_BALANCE_WHOCCUPANCY_SUMMARY]
+      WHERE [WarehouseGroup] = 'Sukabumi'
+  `;
+
+      if (!data.length) {
+        return res.status(404).json({
+          errors: [
+            generateError({
+              code: 404,
+              title: 'Data not found',
+              description: 'Data not found',
+              id: generateRandomString(10),
+              status: 404,
+              timestamp: new Date().toISOString(),
+            }),
+          ],
+        });
+      }
 
       res.json({
         links,
-        data,
+        data: [
+          {
+            type: 'warehouse',
+            id: '1',
+            attributes: {
+              slug: 'warehouse-occupancy',
+              name: 'Warehouse Occupancy',
+              uom: '%',
+              description: 'Warehouse occupancy (%)',
+              sourceType: 'source',
+              value: {
+                value: data[0].SCORE * 100,
+                last_update: new Date().toISOString(),
+              },
+            },
+          },
+          {
+            type: 'warehouse',
+            id: '2',
+            attributes: {
+              slug: 'rak-availability',
+              name: 'Rak Availability',
+              uom: 'rak',
+              description: 'Warehouse availability (Rak)',
+              sourceType: 'source',
+              value: {
+                value: 0,
+                last_update: null,
+              },
+            },
+          },
+          {
+            type: 'warehouse',
+            id: '3',
+            attributes: {
+              slug: 'pallet-availability',
+              name: 'Pallet Availability',
+              uom: 'pallet',
+              description: 'Warehouse availability (Pallet)',
+              sourceType: 'source',
+              value: {
+                value: 0,
+                last_update: null,
+              },
+            },
+          },
+        ],
       });
     } catch (error) {
       catchResponse(res, error);
