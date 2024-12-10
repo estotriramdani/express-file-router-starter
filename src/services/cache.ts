b909a91a20e1e250be6eec42416dd146e6f4fd36
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-12-10 14:38:17 +0700

    feat: extend cache item time-to-live to 3 hours

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -4,5 +4,5 @@ export const cache = Cache({
   basePath: './.cache', // (optional) Path where cache files are stored (default).
   ns: 'my-namespace', // (optional) A grouping namespace for items.
   hash: 'sha1', // (optional) A hashing algorithm used within the cache key.
-  ttl: 60 * 60, // (optional) A time-to-live (in secs) on how long an item remains cached.
+  ttl: 60 * 60 * 3, // (optional) A time-to-live (in secs) on how long an item remains cached.
 });
