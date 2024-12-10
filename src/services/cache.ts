895ba3a2304953555614dc1dffe9235c9e0beb29
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-12-10 15:18:57 +0700

    feat: implement initial cache service structure with placeholder methods

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -1,4 +1,30 @@
-import Cache from 'file-system-cache';
+// @ts-ignore
+// import Cache from 'file-system-cache';
+
+// export const cache = Cache({
+//   basePath: './.cache', // (optional) Path where cache files are stored (default).
+//   ns: 'my-namespace', // (optional) A grouping namespace for items.
+//   hash: 'sha1', // (optional) A hashing algorithm used within the cache key.
+//   ttl: 60 * 60 * 3, // (optional) A time-to-live (in secs) on how long an item remains cached.
+// });
+
+// TODO: Implement cache service
+const Cache = (options) => {
+  return {
+    set: async (...all) => {
+      return;
+    },
+    get: async (...all) => {
+      return null;
+    },
+    remove: async (...all) => {
+      return;
+    },
+    clear: async () => {
+      return;
+    },
+  };
+}
 
 export const cache = Cache({
   basePath: './.cache', // (optional) Path where cache files are stored (default).
