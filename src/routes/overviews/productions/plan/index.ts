82c7faa5fead378c727dadf573c7c4ecb2e93af8
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-12-11 14:24:40 +0700

    feat: implement monthly production plan calculation and update date range logic

--- src/routes/overviews/productions/plan/index.ts
+++ src/routes/overviews/productions/plan/index.ts
@@ -5,6 +5,7 @@ import { catchResponse } from '@/utils/response';
 import { df_db, dwh_db } from '@/lib/db';
 import moment from 'moment';
 import { getFinishGoodWithPlan } from '@/services/get-finish-good';
+import { allLines } from '@/constants';
 
 interface ResponseDataFinishGood {
   type: string;
@@ -31,10 +32,53 @@ export const get = [
         self: process.env.SELF_URL + req.originalUrl,
       };
 
-      const sevenDaysBefore = moment().subtract(7, 'days').format('YYYY-MM-DD 00:00:00');
-      const sevenDaysAfter = moment().add(7, 'days').format('YYYY-MM-DD 23:59:59');
+      if (unit === 'monthly') {
+        const finalData: ResponseDataFinishGood[] = [];
+        const weeks = generateWeeks(moment().format('YYYY-MM-DD'));
+        for (let i = 0; i < weeks.length; i++) {
+          const { start, end } = weeks[i];
+          const combined = await getFinishGoodWithPlan(start, end);
+          allLines.forEach((line) => {
+            let actual_production = 0;
+            let target_production = 0;
+            const filtered = combined.filter((item) => item.slug === line.slug);
+            filtered.forEach((item) => {
+              actual_production += item.actual_production;
+              target_production += item.target_production;
+            });
+            finalData.push({
+              type: 'production-plan',
+              id: `${line.slug}_${start}`,
+              attributes: {
+                slug: line.slug,
+                name: line.name,
+                date: start,
+                value: {
+                  actual_production,
+                  target_production,
+                  last_update: null,
+                },
+              },
+            });
+          });
+        }
 
-      const combined = await getFinishGoodWithPlan(sevenDaysBefore, sevenDaysAfter);
+        res.json({
+          links,
+          data: finalData,
+          _meta: {
+            date: {
+              start: weeks[0].start,
+              end: weeks[weeks.length - 1].end,
+            },
+          },
+        });
+      }
+
+      const startDate = moment().subtract(7, 'days').format('YYYY-MM-DD 00:00:00');
+      const endDate = moment().add(8, 'days').format('YYYY-MM-DD 23:59:59');
+
+      const combined = await getFinishGoodWithPlan(startDate, endDate);
 
       const finalData: ResponseDataFinishGood[] = combined.map((item) => {
         const isAfter = moment(item.date).isAfter(moment());
@@ -54,15 +98,13 @@ export const get = [
         };
       });
 
-      const weeks = generateWeeks('2024-12');
-
       res.json({
         links,
         data: finalData,
         _meta: {
           date: {
-            start: new Date(sevenDaysBefore).toISOString(),
-            end: new Date(sevenDaysAfter).toISOString(),
+            start: new Date(startDate).toISOString(),
+            end: new Date(endDate).toISOString(),
           },
         },
       });
