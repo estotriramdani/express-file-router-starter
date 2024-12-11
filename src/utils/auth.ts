2976baffe266f92b66299cb6dff2a914ce2f5bf8
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-12-11 08:02:54 +0700

    feat: extend access token expiration time from 1h to 8h

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,6 +4,6 @@ import jwt from 'jsonwebtoken';
 export function generateAccessToken(user: LoginDataAttributes) {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
     // expiresIn: process.env.MODE === 'development' ? '1d' : '1h',
-    expiresIn: '1h',
+    expiresIn: '8h',
   });
 }
