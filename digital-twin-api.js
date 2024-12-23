998a3e19cff1e955d24763ed089138587fe4608f
Esto Triramdani Nurlustiawan <estolagi@gmail.com>
2024-12-23 13:22:06 +0700

    feat: update production start command to use Bun

--- digital-twin-api.js
+++ digital-twin-api.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-const child = exec('npm run start:prod', { cwd: __dirname, windowsHide: true });
+const child = exec('npm run start:bun:prod', { cwd: __dirname, windowsHide: true });
 
 child.stdout.pipe(process.stdout);
 child.stderr.pipe(process.stderr);
