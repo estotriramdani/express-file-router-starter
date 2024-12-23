f81265ccc81fb2a74960b0e27a647573eb8ef8a9
nkusworo <nkusworo@aio.co.id>
2024-12-23 11:29:14 +0700

    checkpoint endpoint for kejayan 23/12/2024

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,6 +6,10 @@ import { PrismaClient as PrismaClientScada } from '@/generated/scada_db';
 import { PrismaClient as PrismaClientDf } from '@/generated/aio_iot_engineering_db';
 import { PrismaClient as PrismaClientAioIot } from '@/generated/aio_iot_db';
 import { PrismaClient as PrismaClientDwh } from '@/generated/dwh_db';
+import { PrismaClient as PrismaClientAioIotOci1 } from '@/generated/aio_iot_oci1';
+import { PrismaClient as PrismaClientAioIotOci2 } from '@/generated/aio_iot_oci2';
+import { PrismaClient as PrismaClientNodeRedKjy } from '@/generated/node_red_kjy';
+import { PrismaClient as PrismaClientScadaDb1Kjy } from '@/generated/scada_db1_kjy';
 
 export const employment_db = new PrismaClientEmployment();
 
@@ -22,3 +26,11 @@ export const df_db = new PrismaClientDf();
 export const aio_iot_db = new PrismaClientAioIot();
 
 export const dwh_db = new PrismaClientDwh();
+
+export const aio_iot_oci1 = new PrismaClientAioIotOci1();
+
+export const aio_iot_oci2 = new PrismaClientAioIotOci2();
+
+export const node_red_kjy = new PrismaClientNodeRedKjy();
+
+export const scada_db1_kjy = new PrismaClientScadaDb1Kjy();
---
+++ src/routes/kejayan/lines/[lineSlug]/sub-areas/index.ts
@@ -0,0 +1,64 @@
+import type { Request, Response } from 'express';
+import { authenticateJWT } from '@/middlewares/bearerToken';
+import { catchResponse } from '@/utils/response';
+import { digital_twin_db } from '@/lib/db';
+import { getStatusLineKjy } from '@/services/get-status-line';
+
+export const get = [
+  // authenticateJWT,
+  async (req: Request, res: Response) => {
+    try {
+      const links = {
+        self: process.env.SELF_URL + req.originalUrl,
+      };
+
+      let dataLine: any;
+      if (req.params.lineSlug.includes('1')) {
+        dataLine = await getStatusLineKjy('aio_iot_oci1.v_digital_twin');
+      } else if (req.params.lineSlug.includes('2')) {
+        dataLine = await getStatusLineKjy('aio_iot_oci2.v_digital_twin');
+      }
+
+      const data = await digital_twin_db.mst_overview.findMany({
+        where: {
+          area: 'PRODUCTION_KJY',
+          slug: {
+            startsWith: `${req.params.lineSlug}-`, // Filters slugs starting with "line-oc1-"
+          },
+        }
+      });
+
+      const newData: any[] = [];
+
+      for (let i = 0; i < data.length; i++) {
+        const element: Record<string, any> = { ...data[i] };
+        element.value = { value: dataLine.value[0]?.status === 'PRODUCTION' ? 1 : 0, last_update: dataLine.last_update }
+        newData.push(element);
+      }
+
+      const withAttributes = newData
+        .map((item) => {
+          return {
+            type: 'line-sub-areas',
+            id: item.id,
+            attributes: {
+              slug: item.slug,
+              name: item.name,
+              uom: item.uom,
+              description: item.description,
+              placeholder: item.placeholder,
+              sourceType: item.sourceType,
+              value: item.value,
+            },
+          };
+        });
+
+      res.json({
+        links: links,
+        data: withAttributes,
+      });
+    } catch (error) {
+      catchResponse(res, error);
+    }
+  },
+];
