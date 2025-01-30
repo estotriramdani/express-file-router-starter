688eb308eccf73ad3228cc124a1c19fad15734ef
Esto Triramdani N <estolagi@gmail.com>
2025-01-30 13:08:39 +0700

    feat: add CRUD operations for users in todos route

---
+++ src/routes/todos/index.ts
@@ -0,0 +1,23 @@
+import { printTimestamp } from '@/middlewares/printTimestamp';
+import { Request, Response } from 'express';
+
+// GET `/users`
+export const get = (req: Request, res: Response) => {
+  const users = [
+    { id: 1, name: 'Name1' },
+    { id: 2, name: 'Name2' },
+  ];
+  res.json(users);
+};
+
+export const post = [
+  printTimestamp,
+  (req: Request, res: Response) => {
+    const newUser = {
+      id: Date.now(),
+      name: req.body.name,
+    };
+    // Normally, you would save this user to a database
+    res.status(201).json(newUser);
+  },
+];
