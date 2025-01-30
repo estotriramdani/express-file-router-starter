7e65c70b5b4bc871ac91d15823fbfa656cf47e1f
Esto Triramdani N <estolagi@gmail.com>
2025-01-30 13:49:56 +0700

    feat: implement JWT authentication in login route and add new database URL to env example

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,8 +1,29 @@
 import { Response, Request } from 'express';
 import { generateError, generateRandomString } from '@/utils';
+import md5 from 'md5';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || '';
 
 export const post = async (req: Request, res: Response) => {
   try {
+    const { username, password } = req.body;
+
+    const whereClause = {
+      username: username.length === 4 ? username : username.substr(username.length - 4),
+      password: md5(password),
+    };
+
+    const dataUser = {
+      id: 1,
+      username: 'admin',
+      email: '',
+    };
+
+    const token = jwt.sign(dataUser, JWT_SECRET, {
+      expiresIn: '24h',
+    });
+
     return res.json({});
   } catch (error) {
     return res.status(500).json({
